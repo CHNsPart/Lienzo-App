529a207b8c068ea08c8dac9db7efb43c936a8fc1
chnspart <imchn24@gmail.com>
2024-10-09 02:20:13 -0400

    Next config route added

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'lienzo-app.vercel.app' ],
+    domains: ['localhost', 'lienzo-app.vercel.app'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: '/api/auth/:path*',
+      },
+    ];
   },
 };
+
 export default nextConfig;
